fa7b1ff206a20c50960102f9e58653fdf2c8d553
Yong Choi <ychoi.wikibook@gmail.com>
2023-12-05 16:18:00 +0900

    Update sample.py

--- 01_setting/sample.py
+++ 01_setting/sample.py
@@ -1,12 +1,12 @@
 import json
-import openai  #←OpenAIが用意しているPythonパッケージをインポートする
+import openai  #← OpenAI에서 제공하는 Python 패키지 가져오기
 
-response = openai.ChatCompletion.create(  #←OpenAIのAPIを呼び出すことで、言語モデルを呼び出している
-    model="gpt-3.5-turbo",  #←呼び出す言語モデルの名前
+response = openai.ChatCompletion.create(  #←OpenAI API를 호출하여 언어 모델을 호출합니다.
+    model="gpt-3.5-turbo",  #← 호출할 언어 모델의 이름
     messages=[
         {
             "role": "user",
-            "content": "iPhone8のリリース日を教えて"  #←入力する文章(プロンプト)
+            "content": "iPhone8 출시일을 알려주세요"  #←입력할 문장(프롬프트)
         },
     ]
 )
