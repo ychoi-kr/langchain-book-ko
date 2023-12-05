b3491bd2751e256e2538901ca8503502bf7c23ca
Yong Choi <ychoi.wikibook@gmail.com>
2023-12-05 16:29:25 +0900

    Update chat_model_streaming.py

--- 02_mode_io/chat_model_streaming.py
+++ 02_mode_io/chat_model_streaming.py
@@ -3,11 +3,11 @@ from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage
 
 chat = ChatOpenAI(
-    streaming=True,  #← streamingをTrueに設定し、ストリーミングモードで実行
+    streaming=True,  #← streaming을 True로 설정하여 스트리밍 모드로 실행
     callbacks=[
-        StreamingStdOutCallbackHandler()  #← StreamingStdOutCallbackHandlerをコールバックとして設定
+        StreamingStdOutCallbackHandler()  #← StreamingStdOutCallbackHandler를 콜백으로 설정
     ]
 )
-resp = chat([ #← リクエストを送信
-    HumanMessage(content="おいしいステーキの焼き方を教えて")
+resp = chat([ #← 요청 보내기
+    HumanMessage(content="맛있는 스테이크 굽는 법을 알려주세요")
 ])
