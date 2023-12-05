bd66bd02e18ca2c787845d2cba30214f4ae16b68
Yong Choi <ychoi.wikibook@gmail.com>
2023-12-05 16:39:08 +0900

    Update datetime_output_parser.py

--- 02_mode_io/datetime_output_parser.py
+++ 02_mode_io/datetime_output_parser.py
@@ -1,9 +1,9 @@
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
-from langchain.output_parsers import DatetimeOutputParser  #← Output Parser인 DatetimeOutputParser를 가져옵니다.
+from langchain.output_parsers import DatetimeOutputParser  #← Output Parser인 DatetimeOutputParser를 가져오기
 from langchain.schema import HumanMessage
 
-output_parser = DatetimeOutputParser() #← DatetimeOutputParser를 초기화한다.
+output_parser = DatetimeOutputParser() #← DatetimeOutputParser를 초기화
 
 chat = ChatOpenAI(model="gpt-3.5-turbo", )
 
@@ -16,6 +16,6 @@ result = chat(
     ]
 )
 
-output = output_parser.parse(result.content) #← 출력 결과를 분석하여 날짜 및 시간 형식으로 변환한다.
+output = output_parser.parse(result.content) #← 출력 결과를 분석하여 날짜 및 시간 형식으로 변환
 
 print(output)
