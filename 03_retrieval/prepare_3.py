a67162583327e70cf6c1f1521077c29db6ed714e
Yong Choi <ychoi.wikibook@gmail.com>
2023-12-05 17:29:54 +0900

    Update prepare_3.py

--- 03_retrieval/prepare_3.py
+++ 03_retrieval/prepare_3.py
@@ -1,7 +1,7 @@
 from langchain.document_loaders import PyMuPDFLoader
-from langchain.embeddings import OpenAIEmbeddings  #← OpenAIEmbeddingsをインポート
+from langchain.embeddings import OpenAIEmbeddings  #← OpenAIEmbeddings 가져오기
 from langchain.text_splitter import SpacyTextSplitter
-from langchain.vectorstores import Chroma  #← Chromaをインポート
+from langchain.vectorstores import Chroma  #← Chroma 가져오기
 
 loader = PyMuPDFLoader("./sample.pdf")
 documents = loader.load()
@@ -12,17 +12,17 @@ text_splitter = SpacyTextSplitter(
 )
 splitted_documents = text_splitter.split_documents(documents)
 
-embeddings = OpenAIEmbeddings( #← OpenAIEmbeddingsを初期化する
-    model="text-embedding-ada-002" #← モデル名を指定
+embeddings = OpenAIEmbeddings( #← OpenAIEmbeddings를 초기화
+    model="text-embedding-ada-002" #← 모델명을 지정
 )
 
-database = Chroma(  #← Chromaを初期化する
-    persist_directory="./.data",  #← 永続化データの保存先を指定
-    embedding_function=embeddings  #← ベクトル化するためのモデルを指定
+database = Chroma(  #← Chroma를 초기화
+    persist_directory="./.data",  #← 영속화 데이터 저장 위치 지정
+    embedding_function=embeddings  #← 벡터화할 모델을 지정
 )
 
-database.add_documents(  #← ドキュメントをデータベースに追加
-    splitted_documents,  #← 追加するドキュメント
+database.add_documents(  #← 문서를 데이터베이스에 추가
+    splitted_documents,  #← 추가할 문서 지정
 )
 
-print("データベースの作成が完了しました。") #← 完了を通知する
+print("데이터베이스 생성이 완료되었습니다.") #← 완료 알림
