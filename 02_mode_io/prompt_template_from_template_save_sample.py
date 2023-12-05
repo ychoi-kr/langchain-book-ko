909d8d021cf8a265e4a72f6985e46e6625c8ba0d
Yong Choi <ychoi.wikibook@gmail.com>
2023-12-05 16:56:28 +0900

    Update prompt_template_from_template_save_sample.py

--- 02_mode_io/prompt_template_from_template_save_sample.py
+++ 02_mode_io/prompt_template_from_template_save_sample.py
@@ -1,4 +1,4 @@
 from langchain.prompts import PromptTemplate
 
-prompt = PromptTemplate(template="{product}はどこの会社が開発した製品ですか？", input_variables=["product"])
-prompt_json = prompt.save("prompt.json") #← PromptTemplateをJSONに変換する
+prompt = PromptTemplate(template="{product}는 어느 회사에서 개발한 제품인가요？", input_variables=["product"])
+prompt_json = prompt.save("prompt.json") #← PromptTemplate를 JSON으로 변환
