5db248ac4459f33f990f15a8bba97b9e56bec1dc
Yong Choi <ychoi.wikibook@gmail.com>
2023-12-05 16:31:55 +0900

    Update datetime_output_parser.py

--- 02_mode_io/datetime_output_parser.py
+++ 02_mode_io/datetime_output_parser.py
@@ -1,21 +1,21 @@
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
-from langchain.output_parsers import DatetimeOutputParser  #← Output ParserであるDatetimeOutputParserをインポート
+from langchain.output_parsers import DatetimeOutputParser  #← Output Parser인 DatetimeOutputParser를 가져옵니다.
 from langchain.schema import HumanMessage
 
-output_parser = DatetimeOutputParser() #← DatetimeOutputParserを初期化
+output_parser = DatetimeOutputParser() #← DatetimeOutputParser를 초기화한다.
 
 chat = ChatOpenAI(model="gpt-3.5-turbo", )
 
-prompt = PromptTemplate.from_template("{product}のリリース日を教えて") #← リリース日を聞く
+prompt = PromptTemplate.from_template("{product}의 출시일을 알려주세요") #← 출시일 물어보기
 
 result = chat(
     [
-        HumanMessage(content=prompt.format(product="iPhone8")),  #← iPhone8のリリース日を聞く
-        HumanMessage(content=output_parser.get_format_instructions()),  #← output_parser.get_format_instructions()を実行し、言語モデルへの指示を追加する
+        HumanMessage(content=prompt.format(product="iPhone8")),  #← iPhone8의 출시일 물어보기
+        HumanMessage(content=output_parser.get_format_instructions()),  #← output_parser.get_format_instructions()를 실행하여 언어모델에 지시사항 추가하기
     ]
 )
 
-output = output_parser.parse(result.content) #← 出力結果を解析して日時形式に変換する
+output = output_parser.parse(result.content) #← 출력 결과를 분석하여 날짜 및 시간 형식으로 변환한다.
 
 print(output)
