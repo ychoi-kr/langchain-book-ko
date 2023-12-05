16cf97158198ebb97e975126195513a5350a3986
Yong Choi <ychoi.wikibook@gmail.com>
2023-12-05 16:54:08 +0900

    Update prompt_and_language_model.py

--- 02_mode_io/prompt_and_language_model.py
+++ 02_mode_io/prompt_and_language_model.py
@@ -2,18 +2,18 @@ from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage
 
-chat = ChatOpenAI(  #←クライアントを作成しchatへ保存
-    model="gpt-3.5-turbo",  #← 呼び出すモデルを指定
+chat = ChatOpenAI(  #← 클라이언트 생성 및 chat에 저장
+    model="gpt-3.5-turbo",  #← 호출할 모델 지정
 )
 
-prompt = PromptTemplate(  #← PromptTemplateを作成する
-    template="{product}はどこの会社が開発した製品ですか？",  #← {product}という変数を含むプロンプトを作成する
+prompt = PromptTemplate(  #← PromptTemplate을 작성
+    template="{product}는 어느 회사에서 개발한 제품인가요？",  #← {product}라는 변수를 포함하는 프롬프트 작성하기
     input_variables=[
-        "product"  #← productに入力する変数を指定する
+        "product"  #← product에 입력할 변수 지정
     ]
 )
 
-result = chat( #← 実行する
+result = chat( #← 실행
     [
         HumanMessage(content=prompt.format(product="iPhone")),
     ]
