790f69a82290e83b8d5a18aa08525de6f493bd38
Yong Choi <ychoi.wikibook@gmail.com>
2023-12-05 16:55:26 +0900

    Update prompt_template_from_template_load_sample.py

--- 02_mode_io/prompt_template_from_template_load_sample.py
+++ 02_mode_io/prompt_template_from_template_load_sample.py
@@ -1,5 +1,5 @@
 from langchain.prompts import load_prompt
 
-loaded_prompt = load_prompt("prompt.json") #← JSONからPromptTemplateを読み込む
+loaded_prompt = load_prompt("prompt.json") #← JSON에서 PromptTemplate를 로드
 
-print(loaded_prompt.format(product="iPhone")) #← PromptTemplateを使って文章を生成する
+print(loaded_prompt.format(product="iPhone")) #← PromptTemplate를 사용해 문장 생성
