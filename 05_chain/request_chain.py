075ceb6eb1be6bd5cd4880ced6bbde4a9b4d7db7
Yong Choi <ychoi.wikibook@gmail.com>
2023-12-06 16:25:10 +0900

    Update request_chain.py

--- 05_chain/request_chain.py
+++ 05_chain/request_chain.py
@@ -4,12 +4,12 @@ from langchain.prompts import PromptTemplate
 
 chat = ChatOpenAI()
 
-prompt = PromptTemplate( #← PromptTemplateを初期化
+prompt = PromptTemplate( #← PromptTemplate을 초기화
     input_variables=["query",
                      "requests_result"],
-    template="""以下の文章を元に質問に答えてください。
-文章: {requests_result}
-質問: {query}""",
+    template="""아래 문장을 바탕으로 질문에 답해 주세요.
+문장: {requests_result}
+질문: {query}""",
 )
 
 llm_chain = LLMChain(
@@ -18,11 +18,11 @@ llm_chain = LLMChain(
     verbose=True,
 )
 
-chain = LLMRequestsChain(  #← LLMRequestsChainを初期化
-    llm_chain=llm_chain,  #← llm_chainにはLLMChainを指定
+chain = LLMRequestsChain(  #← LLMRequestsChain을 초기화
+    llm_chain=llm_chain,  #← llm_chainにはLLMChain을 지정
 )
 
 print(chain({
-    "query": "東京の天気について教えて",
+    "query": "도쿄의 날씨를 알려주세요",
     "url": "https://www.jma.go.jp/bosai/forecast/data/overview_forecast/130000.json",
 }))
