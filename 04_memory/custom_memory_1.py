e6c0e79e89ee54cc27feb5a208bc716e2a908554
Yong Choi <ychoi.wikibook@gmail.com>
2023-12-06 11:55:54 +0900

    Update custom_memory_1.py

--- 04_memory/custom_memory_1.py
+++ 04_memory/custom_memory_1.py
@@ -1,7 +1,7 @@
 import chainlit as cl
 from langchain.chains import ConversationChain
 from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferWindowMemory  # ConversationBufferWindowMemoryをインポート
+from langchain.memory import ConversationBufferWindowMemory  # ConversationBufferWindowMemory 가져오기
 
 chat = ChatOpenAI(
     model="gpt-3.5-turbo"
@@ -9,7 +9,7 @@ chat = ChatOpenAI(
 
 memory = ConversationBufferWindowMemory(
     return_messages=True,
-    k=3 # 3往復分のメッセージを記憶する
+    k=3 # 3번 주고받은 메시지를 기억
 )
 
 chain = ConversationChain(
@@ -19,17 +19,17 @@ chain = ConversationChain(
 
 @cl.on_chat_start
 async def on_chat_start():
-    await cl.Message(content="私は会話の文脈を考慮した返答ができるチャットボットです。メッセージを入力してください。").send()
+    await cl.Message(content="저는 대화의 맥락을 고려해 답변할 수 있는 채팅봇입니다. 메시지를 입력하세요.").send()
 
 @cl.on_message
 async def on_message(message: str):
-    messages = chain.memory.load_memory_variables({})["history"] # 保存されているメッセージを取得する
+    messages = chain.memory.load_memory_variables({})["history"] # 저장된 메시지 가져오기
 
-    print(f"保存されているメッセージの数: {len(messages)}" # 保存されているメッセージの数を表示する
+    print(f"保存されているメッセージの数: {len(messages)}" # 저장된 메시지 개수를 표시
           )
 
-    for saved_message in messages: # 保存されているメッセージを1つずつ取り出す
-        print(saved_message.content # 保存されているメッセージを表示する
+    for saved_message in messages: # 저장된 메시지를 1개씩 불러옴
+        print(saved_message.content # 저장된 메시지를 표시
               )
 
     result = chain(message)
