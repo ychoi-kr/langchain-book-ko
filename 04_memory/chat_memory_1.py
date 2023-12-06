0d916d7be7543633c51941a3e91eabb63e1c3efa
Yong Choi <ychoi.wikibook@gmail.com>
2023-12-06 09:44:45 +0900

    Update chat_memory_1.py

--- 04_memory/chat_memory_1.py
+++ 04_memory/chat_memory_1.py
@@ -1,38 +1,38 @@
 import chainlit as cl
 from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferMemory  #← ConversationBufferMemoryをインポート
+from langchain.memory import ConversationBufferMemory  #← ConversationBufferMemory 가져오기
 from langchain.schema import HumanMessage
 
 chat = ChatOpenAI(
     model="gpt-3.5-turbo"
 )
 
-memory = ConversationBufferMemory( #← メモリを初期化
+memory = ConversationBufferMemory( #← 메모리 초기화
     return_messages=True
 )
 
 @cl.on_chat_start
 async def on_chat_start():
-    await cl.Message(content="私は会話の文脈を考慮した返答ができるチャットボットです。メッセージを入力してください。").send()
+    await cl.Message(content="저는 대화의 맥락을 고려해 답변할 수 있는 채팅봇입니다. 메시지를 입력하세요.").send()
 
 @cl.on_message
 async def on_message(message: str):
-    memory_message_result = memory.load_memory_variables({}) #← メモリの内容を取得
+    memory_message_result = memory.load_memory_variables({}) #← 메모리 내용을 로드
 
-    messages = memory_message_result['history'] #← メモリの内容からメッセージのみを取得
+    messages = memory_message_result['history'] #← 메모리 내용에서 메시지만 얻음
 
-    messages.append(HumanMessage(content=message)) #← ユーザーからのメッセージを追加
+    messages.append(HumanMessage(content=message)) #← 사용자의 메시지를 추가
 
-    result = chat( #← Chat modelsを使って言語モデルを呼び出す
+    result = chat( #← Chat models를 사용해 언어 모델을 호출
         messages
     )
 
-    memory.save_context(  #← メモリにメッセージを追加
+    memory.save_context(  #← 메모리에 메시지를 추가
         {
-            "input": message,  #← ユーザーからのメッセージをinputとして保存
+            "input": message,  #← 사용자의 메시지를 input으로 저장
         },
         {
-            "output": result.content,  #← AIからのメッセージをoutputとして保存
+            "output": result.content,  #← AI의 메시지를 output으로 저장
         }
     )
-    await cl.Message(content=result.content).send() #← AIからのメッセージを送信
+    await cl.Message(content=result.content).send() #← AI의 메시지를 송신
