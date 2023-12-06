b4ff92bacaa7da9b8315136cc99775f366da2f64
Yong Choi <ychoi.wikibook@gmail.com>
2023-12-06 09:58:54 +0900

    Update chat_memory_4.py

--- 04_memory/chat_memory_4.py
+++ 04_memory/chat_memory_4.py
@@ -12,46 +12,46 @@ chat = ChatOpenAI(
 @cl.on_chat_start
 async def on_chat_start():
     thread_id = None
-    while not thread_id: #← スレッドIDが入力されるまで繰り返す
-        res = await cl.AskUserMessage(content="私は会話の文脈を考慮した返答ができるチャットボットです。スレッドIDを入力してください。", timeout=600).send() #← AskUserMessageを使ってスレッドIDを入力
+    while not thread_id: #← 스레드 ID가 입력될 때까지 반복
+        res = await cl.AskUserMessage(content="저는 대화의 맥락을 고려해 답변할 수 있는 채팅봇입니다. 스레드 ID를 입력하세요.", timeout=600).send() #← AskUserMessage를 사용해 스레드 ID 입력
         if res:
             thread_id = res['content']
 
-    history = RedisChatMessageHistory(  #← 新しくチャットが始まるたびに初期化するようにon_chat_startに移動
-        session_id=thread_id,  #← スレッドIDをセッションIDとして指定
+    history = RedisChatMessageHistory(  #← 새로 채팅이 시작될 때마다 초기화하도록 on_chat_start로 이동
+        session_id=thread_id,  #← 스레드 ID를 세션 ID로 지정
         url=os.environ.get("REDIS_URL"),
     )
 
-    memory = ConversationBufferMemory( #← 新しくチャットが始まるたびに初期化するようにon_chat_startに移動
+    memory = ConversationBufferMemory( #← 새로 채팅이 시작될 때마다 초기화하도록 on_chat_start로 이동
         return_messages=True,
         chat_memory=history,
     )
 
-    chain = ConversationChain( #← 新しくチャットが始まるたびに初期化するようにon_chat_startに移動
+    chain = ConversationChain( #← 새로 채팅이 시작될 때마다 초기화하도록 on_chat_start로 이동
         memory=memory,
         llm=chat,
     )
 
-    memory_message_result = chain.memory.load_memory_variables({}) #← メモリの内容を取得
+    memory_message_result = chain.memory.load_memory_variables({}) #← 메모리 내용 가져오기
 
     messages = memory_message_result['history']
 
     for message in messages:
-        if isinstance(message, HumanMessage): #← ユーザーからのメッセージかどうかを判定
-            await cl.Message( #← ユーザーからのメッセージの場合はauthorUserを指定して送信
+        if isinstance(message, HumanMessage): #← 사용자가 보낸 메시지인지 판단
+            await cl.Message( #← 사용자 메시지이면 authorUser를 지정해 송신
                 author="User",
                 content=f"{message.content}",
             ).send()
         else:
-            await cl.Message( #← AIからのメッセージの場合はChatBotを指定して送信
+            await cl.Message( #← AI의 메시지이면 ChatBot을 지정해 송신
                 author="ChatBot",
                 content=f"{message.content}",
             ).send()
-    cl.user_session.set("chain", chain) #← 履歴をセッションに保存
+    cl.user_session.set("chain", chain) #← 기록을 세션에 저장
 
 @cl.on_message
 async def on_message(message: str):
-    chain = cl.user_session.get("chain") #← セッションから履歴を取得
+    chain = cl.user_session.get("chain") #← 세션에서 기록을 가져오기
 
     result = chain(message)
 
