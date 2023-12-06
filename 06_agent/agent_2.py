3f4268bbaec590ae44cb7cd772ea5e916c70a02d
Yong Choi <ychoi.wikibook@gmail.com>
2023-12-06 19:22:29 +0900

    Update agent_2.py

--- 06_agent/agent_2.py
+++ 06_agent/agent_2.py
@@ -1,6 +1,6 @@
-from langchain.agents import AgentType, initialize_agent, load_tools  #←load_toolsをインポートを追加
+from langchain.agents import AgentType, initialize_agent, load_tools  #←load_tools 가져오기를 추가
 from langchain.chat_models import ChatOpenAI
-from langchain.tools.file_management import WriteFileTool  #←ファイル書き込みできるToolをインポート
+from langchain.tools.file_management import WriteFileTool  #←파일 쓰기를 할 수 있는 Tool을 가져오기
 
 chat = ChatOpenAI(
     temperature=0,
@@ -10,22 +10,22 @@ chat = ChatOpenAI(
 tools = load_tools(
     [
         "requests_get",
-        "serpapi" #←serpapiを追加
+        "serpapi" #←serpapi를 추가
     ],
     llm=chat
 )
 
-tools.append(WriteFileTool( #←ファイル書き込みできるToolを追加
+tools.append(WriteFileTool( #←파일 쓰기를 할 수 있는 Tool을 추가
     root_dir="./"
 ))
 
 agent = initialize_agent(
     tools,
     chat,
-    agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,  #←Agentのタイプを変更
+    agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,  #←Agent 유형을 변경
     verbose=True
 )
 
-result = agent.run("北海道の名産品を調べて日本語でresult.txtというファイルに保存してください。") #←実行結果をファイルに保存するように指示
+result = agent.run("제주도 특산품을 검색해 result.txt 파일에 한국어로 저장하세요.") #←실행 결과를 파일에 저장하도록 지시
 
-print(f"実行結果: {result}")
+print(f"실행 결과: {result}")
