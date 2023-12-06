a18e1510f3addce58c303e36c65befa881fee8ac
Yong Choi <ychoi.wikibook@gmail.com>
2023-12-06 17:15:53 +0900

    Update sequential_chain.py

--- 05_chain/sequential_chain.py
+++ 05_chain/sequential_chain.py
@@ -4,29 +4,29 @@ from langchain.prompts import PromptTemplate
 
 chat = ChatOpenAI(model="gpt-3.5-turbo")
 
-write_article_chain = LLMChain( #← 記事を書くLLMChainを作成する
+write_article_chain = LLMChain( #← 기사를 쓰는 LLMChain 만들기
     llm=chat,
     prompt=PromptTemplate(
-        template="{input}についての記事を書いてください。",
+        template="{input}에 관한 기사를 써주세요.",
         input_variables=["input"],
     ),
 )
 
-translate_chain = LLMChain( #← 翻訳するLLMChainを作成する
+translate_chain = LLMChain( #← 번역하는 LLMChain을 생성
     llm=chat,
     prompt=PromptTemplate(
-        template="以下の文章を英語に翻訳してください。\n{input}",
+        template="다음 문장을 영어로 번역해 주세요.\n{input}",
         input_variables=["input"],
     ),
 )
 
-sequential_chain = SimpleSequentialChain( #← SimpleSequentialChainを作成する
-    chains=[ #← 実行するChainを指定する
+sequential_chain = SimpleSequentialChain( #← SimpleSequentialChain을 생성
+    chains=[ #← 실행할 Chain을 지정
         write_article_chain,
         translate_chain,
     ]
 )
 
-result = sequential_chain.run("エレキギターの選び方")
+result = sequential_chain.run("일렉트릭 기타 선택 방법")
 
 print(result)
