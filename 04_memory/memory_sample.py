e131bd568c6ccf00992bbbe4d8a4d28a5ba02231
Yong Choi <ychoi.wikibook@gmail.com>
2023-12-06 15:00:57 +0900

    Update memory_sample.py

--- 04_memory/memory_sample.py
+++ 04_memory/memory_sample.py
@@ -1,24 +1,24 @@
 from langchain.memory import ConversationBufferMemory 
-memory = ConversationBufferMemory( #← メモリを初期化
+memory = ConversationBufferMemory( #← 메모리 초기화
     return_messages=True,
 ) 
-memory.save_context( #← メモリにメッセージを追加
+memory.save_context( #← 메모리에 메시지를 추가
     {
-        "input": "こんにちは！"
+        "input": "안녕하세요!"
     },
     {
-        "output": "こんにちは！お元気ですか？何か質問があればどうぞお知らせください。どのようにお手伝いできますか？"
+        "output": "안녕하세요! 잘 지내고 계신가요? 궁금한 점이 있으면 알려 주세요. 어떻게 도와드릴까요?"
     }
 )
-memory.save_context( #← メモリにメッセージを追加
+memory.save_context( #← 메모리에 메시지를 추가
     {
-        "input": "今日はいい天気ですね"
+        "input": "오늘 날씨가 좋네요"
     },
     {
-        "output": "私はAIなので、実際の天候を感じることはできませんが、いい天気の日は外出や活動を楽しむのに最適ですね！"
+        "output": "저는 AI이기 때문에 실제 날씨를 느낄 수는 없지만, 날씨가 좋은 날은 외출이나 활동을 즐기기에 좋은 날입니다!"
     }
 )
 
 print(
-    memory.load_memory_variables({}) #← メモリの内容を確認
+    memory.load_memory_variables({}) #← 메모리 내용을 확인
 )
