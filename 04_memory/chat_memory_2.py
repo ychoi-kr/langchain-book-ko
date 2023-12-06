b53a16286f5d422657c498afdea346c6185e0f34
Yong Choi <ychoi.wikibook@gmail.com>
2023-12-06 09:48:45 +0900

    Update chat_memory_2.py

--- 04_memory/chat_memory_2.py
+++ 04_memory/chat_memory_2.py
@@ -1,5 +1,5 @@
 import chainlit as cl
-from langchain.chains import ConversationChain  #← ConversationChainを追加
+from langchain.chains import ConversationChain  #← ConversationChain을 가져오기
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 
@@ -11,20 +11,20 @@ memory = ConversationBufferMemory(
     return_messages=True
 )
 
-chain = ConversationChain( #← ConversationChainを初期化
+chain = ConversationChain( #← ConversationChain을 초기화
     memory=memory,
     llm=chat,
 )
 
 @cl.on_chat_start
 async def on_chat_start():
-    await cl.Message(content="私は会話の文脈を考慮した返答をできるチャットボットです。メッセージを入力してください。").send()
+    await cl.Message(content="저는 대화의 맥락을 고려해 답변할 수 있는 채팅봇입니다. 메시지를 입력하세요.").send()
 
 @cl.on_message
 async def on_message(message: str):
 
-    result = chain( #← ConversationChainを使って言語モデルを呼び出す
-        message #← ユーザーからのメッセージを引数に指定
+    result = chain( #← ConversationChain을 사용해 언어 모델을 호출
+        message #← 사용자 메시지를 인수로 지정
     )
 
     await cl.Message(content=result["response"]).send()
