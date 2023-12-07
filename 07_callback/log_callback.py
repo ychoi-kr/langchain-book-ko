266b81160dc5084eb4b1639eadf2ae43f6883a63
Yong Choi <ychoi.wikibook@gmail.com>
2023-12-07 11:06:10 +0900

    Update log_callback.py

--- 07_callback/log_callback.py
+++ 07_callback/log_callback.py
@@ -1,27 +1,27 @@
-from langchain.callbacks.base import BaseCallbackHandler #← BaseCallbackHandlerをインポート
+from langchain.callbacks.base import BaseCallbackHandler #← BaseCallbackHandler 가져오기
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage
 
 
-class LogCallbackHandler(BaseCallbackHandler): #← Callbackを作成する
+class LogCallbackHandler(BaseCallbackHandler): #← Callback을 생성
 
-    def on_chat_model_start(self, serialized, messages, **kwargs): #← Chat modelsの実行開始時に呼び出される処理を定義する
-        print("Chat modelsの実行を開始します....")
-        print(f"入力: {messages}")
+    def on_chat_model_start(self, serialized, messages, **kwargs): #← Chat models 실행 시작 시 호출되는 처리를 정의
+        print("Chat models 실행 시작....")
+        print(f"입력: {messages}")
 
-    def on_chain_start(self, serialized, inputs, **kwargs): #← Chainの実行開始時に呼び出される処理を定義する
-        print("Chainの実行を開始します....")
-        print(f"入力: {inputs}")
+    def on_chain_start(self, serialized, inputs, **kwargs): #← Chain 실행 시작 시 호출되는 처리를 정의
+        print("Chain 실행 시작....")
+        print(f"입력: {inputs}")
 
 chat = ChatOpenAI(
     model="gpt-3.5-turbo",
-    callbacks=[ #← Chat modelsの初期化時にCallbackを指定する
-        LogCallbackHandler() #← 作成したLogCallbackHandlerを指定する
+    callbacks=[ #← Chat models 초기화 시 Callback을 지정
+        LogCallbackHandler() #← 생성한 LogCallbackHandler를 지정
     ]
 )
 
 result = chat([
-    HumanMessage(content="こんにちは！"),
+    HumanMessage(content="안녕하세요!"),
 ])
 
 print(result.content)
