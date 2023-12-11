216fba7524e614d3e34c548b6c7ff008df56d0be
Yong Choi <sk8er.choi@gmail.com>
2023-12-11 18:34:12 +0900

    Update sample_wikipedia.py

--- 03_retrieval/sample_wikipedia.py
+++ 03_retrieval/sample_wikipedia.py
@@ -4,10 +4,10 @@ retriever = WikipediaRetriever(  #← WikipediaRetriever를 초기화
     lang="ko",  #← Wikipedia의 언어를 지정
 )
 documents = retriever.get_relevant_documents( #← Wikipedia에서 관련 문서를 가져옴
-    "대규모 언어 모델" #← 검색할 키워드를 지정
+    "대형 언어 모델" #← 검색할 키워드를 지정
 )
 
-print(f"검색 결과: {len(documents)}件") #← 검색 결과 건수를 표시
+print(f"검색 결과: {len(documents)}건") #← 검색 결과 건수를 표시
 
 for document in documents:
     print("---------------검색한 메타데이터---------------")
